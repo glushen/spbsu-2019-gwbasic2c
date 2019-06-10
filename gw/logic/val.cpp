a05827e63a1b3084cd2415fa27ea89007d3f1629
Pavel Glushen <pavel@glushen.ru>
2019-06-10 20:07:12 +0300

    Fixed gw/logic/val.cpp

--- gw/logic/val.cpp
+++ gw/logic/val.cpp
@@ -1,5 +1,6 @@
 #include "../core/to_double_or_nan.cpp"
 #include "../core/check_d.cpp"
 double val(const std::string& string) {
-    return check_d(to_double_or_nan(string));
+    double value = to_double_or_nan(string);
+    return value == value ? check_d(value) : 0;
 }
